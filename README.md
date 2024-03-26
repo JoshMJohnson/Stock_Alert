89113f44aa933b4374cd81ff1dae2764c3d06daa
JoshMJohnson <joshjohns21@gmail.com>
2024-03-25 23:20:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stock_alert
+ Provides stock alerts of major changes in the market
---
+++ android/app/src/main/kotlin/com/example/stock_alert/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stock_alert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
