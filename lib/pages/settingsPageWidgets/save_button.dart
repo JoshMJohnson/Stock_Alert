4234175337b24b0436f4a36cb4c802c477a1b3a2
JoshMJohnson <joshjohns21@gmail.com>
2024-04-07 23:07:42 -0500

    created settings classes for widgets

---
+++ lib/pages/settingsPageWidgets/save_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SaveButton extends StatefulWidget {
+  const SaveButton({super.key});
+
+  @override
+  State<SaveButton> createState() => _SaveButtonState();
+}
+
+class _SaveButtonState extends State<SaveButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/settingsPageWidgets/tod_reminders.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TODReminders extends StatefulWidget {
+  const TODReminders({super.key});
+
+  @override
+  State<TODReminders> createState() => _TODRemindersState();
+}
+
+class _TODRemindersState extends State<TODReminders> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
