104018624f2dba98b8b397c7d29a38cff4b3ec01
JoshMJohnson <joshjohns21@gmail.com>
2024-04-17 09:25:37 -0500

    code cleanup

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,4 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:stock_alert/helperClasses/helper_functions.dart';
 
 import 'package:stock_alert/pages/settings.dart';
