28052082d507b51b58f9dcb90a1de293a13e7996
JoshMJohnson <joshjohns21@gmail.com>
2024-04-27 00:13:29 -0500

    initialized settings variables onload up

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,26 @@ Future<void> main() async {
 
   /* loads settings from device preferences */
   String? sortAlgorithm = prefs.getString('watchlistSort') ?? 'Alphabetically';
+  bool? notificationToggledOn = prefs.getBool('notificationToggle') ?? false;
+  double? thresholdValue = prefs.getDouble('thresholdValue') ?? 5.0;
+  int? notificationQuantity = prefs.getInt('notificationQuantity') ?? 3;
+
+  /* loads daily reminder settings */
+  late TimeOfDay notification1;
+  late TimeOfDay notification2;
+  late TimeOfDay notification3;
+
+  int? tod1Hours = prefs.getInt('tod1Hours') ?? 8;
+  int? tod2Hours = prefs.getInt('tod2Hours') ?? 12;
+  int? tod3Hours = prefs.getInt('tod3Hours') ?? 14;
+
+  int? tod1Minutes = prefs.getInt('tod1Minutes') ?? 45;
+  int? tod2Minutes = prefs.getInt('tod2Minutes') ?? 0;
+  int? tod3Minutes = prefs.getInt('tod3Minutes') ?? 15;
+
+  notification1 = TimeOfDay(hour: tod1Hours, minute: tod1Minutes);
+  notification2 = TimeOfDay(hour: tod2Hours, minute: tod2Minutes);
+  notification3 = TimeOfDay(hour: tod3Hours, minute: tod3Minutes);
 
   runApp(MyApp(sortAlgorithm: sortAlgorithm));
 }
