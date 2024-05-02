df4ad4dc694af7702ef23db40757132f94ea13bb
JoshMJohnson <joshjohns21@gmail.com>
2024-05-01 22:56:42 -0500

    removed nullable properties

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,27 @@ import 'pages/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
 
   /* loads settings from device preferences */
-  String? sortAlgorithm = prefs.getString('watchlistSort') ?? 'Alphabetically';
-  bool? notificationToggledOn = prefs.getBool('notificationToggle') ?? false;
-  double? thresholdValue = prefs.getDouble('thresholdValue') ?? 5.0;
-  int? notificationQuantity = prefs.getInt('notificationQuantity') ?? 3;
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String sortAlgorithm = prefs.getString('watchlistSort') ?? 'Alphabetically';
+  bool notificationToggledOn = prefs.getBool('notificationToggle') ?? false;
+  double thresholdValue = prefs.getDouble('thresholdValue') ?? 5.0;
+  int notificationQuantity = prefs.getInt('notificationQuantity') ?? 3;
 
   /* loads daily reminder settings */
   late TimeOfDay notification1;
   late TimeOfDay notification2;
   late TimeOfDay notification3;
 
-  int? tod1Hours = prefs.getInt('tod1Hours') ?? 8;
-  int? tod2Hours = prefs.getInt('tod2Hours') ?? 12;
-  int? tod3Hours = prefs.getInt('tod3Hours') ?? 14;
+  int tod1Hours = prefs.getInt('tod1Hours') ?? 8;
+  int tod2Hours = prefs.getInt('tod2Hours') ?? 12;
+  int tod3Hours = prefs.getInt('tod3Hours') ?? 14;
 
-  int? tod1Minutes = prefs.getInt('tod1Minutes') ?? 45;
-  int? tod2Minutes = prefs.getInt('tod2Minutes') ?? 0;
-  int? tod3Minutes = prefs.getInt('tod3Minutes') ?? 15;
+  int tod1Minutes = prefs.getInt('tod1Minutes') ?? 45;
+  int tod2Minutes = prefs.getInt('tod2Minutes') ?? 0;
+  int tod3Minutes = prefs.getInt('tod3Minutes') ?? 15;
 
   notification1 = TimeOfDay(hour: tod1Hours, minute: tod1Minutes);
   notification2 = TimeOfDay(hour: tod2Hours, minute: tod2Minutes);
