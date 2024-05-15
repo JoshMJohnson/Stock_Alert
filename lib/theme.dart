1b35af97e20d7726af6f4e9fb2e195f8d4f57f01
JoshMJohnson <joshjohns21@gmail.com>
2024-05-14 22:48:34 -0500

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -11,6 +11,11 @@ ThemeData lightMode = ThemeData(
     ));
 
 ThemeData darkMode = ThemeData(
-  brightness: Brightness.dark,
-  fontFamily: 'EBGaramond',
-);
+    brightness: Brightness.dark,
+    fontFamily: 'EBGaramond',
+    colorScheme: ColorScheme.light(
+      background: Colors.grey.shade400,
+      primary: Colors.grey.shade400,
+      secondary: Colors.grey.shade400,
+      tertiary: Colors.grey.shade400,
+    ));
