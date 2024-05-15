02e28419609e79a008c9386c87ec1aec58bc63e8
JoshMJohnson <joshjohns21@gmail.com>
2024-05-14 23:08:28 -0500

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,16 +4,15 @@ ThemeData lightMode = ThemeData(
     brightness: Brightness.light,
     fontFamily: 'EBGaramond',
     colorScheme: ColorScheme.light(
-      background: Colors.grey.shade400,
-      primary: Colors.grey.shade400,
-      secondary: Colors.grey.shade400,
-      tertiary: Colors.grey.shade400,
-    ));
+        background: Colors.grey.shade400,
+        primary: Colors.grey.shade400,
+        secondary: Colors.grey.shade400,
+        tertiary: Colors.grey.shade400));
 
 ThemeData darkMode = ThemeData(
     brightness: Brightness.dark,
     fontFamily: 'EBGaramond',
-    colorScheme: ColorScheme.light(
+    colorScheme: ColorScheme.dark(
       background: Colors.grey.shade400,
       primary: Colors.grey.shade400,
       secondary: Colors.grey.shade400,
