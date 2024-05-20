32d6b7ae0b4285fda93512d76ebacafdd567b16e
JoshMJohnson <joshjohns21@gmail.com>
2024-05-20 15:00:11 -0500

    initialized database repository

---
+++ lib/database_repository.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stock_alert/pages/homePageWidgets/stock_entity.dart';
+
+class DatabaseRepository {
+  /* adds a stock symbol to the watchlist */ // todo
+  void addSymbol() {}
+
+  /* removes a stock symbol from the watchlist */ // todo
+  void removeSymbol() {}
+
+  /* routine update to stock data (ex: PPS/Day Change) */ // todo
+  void updateSymbolData() {}
+
+  /* updates stock symbol toggle to receive updates */ // todo
+  void updateSymbolToggle(StockEntity tickerSymbol) {}
+
+  /* clears the watchlist */ // todo
+  void clearWatchlist() {}
+
+  /* returns stock symbols on the watchlist */ // todo
+  List<StockEntity> getWatchlist() {
+    List<StockEntity> updatedWatchlist = [];
+    return updatedWatchlist;
+  }
+}
