7403d87b3b1aa168e7023511b45dd16f7d29f9c6
JoshMJohnson <joshjohns21@gmail.com>
2024-05-28 22:29:51 -0500

    saved button alert created
    in settings page

--- lib/pages/settingsPageWidgets/save_button.dart
+++ lib/pages/settingsPageWidgets/save_button.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
 class SaveButton extends StatelessWidget {
-  final Function saveButtonHandler;
+  final Function() saveButtonHandler;
   const SaveButton(this.saveButtonHandler, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () async {
+      onPressed: () {
         saveButtonHandler();
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: Text(
+              'Settings Saved',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.headlineMedium!.color,
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            content: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Icon(
+                Icons.check_circle_outline,
+                size: 55,
+                color: Theme.of(context).iconTheme.color,
+              ),
+            ),
+          ),
+        );
       },
       style: ElevatedButton.styleFrom(
         backgroundColor: Theme.of(context).buttonTheme.colorScheme!.background,
