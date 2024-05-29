b1a8054ba699985a61ac20c321c887027c3aca61
JoshMJohnson <joshjohns21@gmail.com>
2024-05-28 22:01:30 -0500

    alert created for clear watchlist in settings

--- lib/pages/settingsPageWidgets/clear_watchlist.dart
+++ lib/pages/settingsPageWidgets/clear_watchlist.dart
@@ -13,9 +13,42 @@ class ClearWatchlist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () async {
-        clearWatchlistHandler();
-      },
+      onPressed: () => showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Clear Watchlist'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Are you sure?'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () => {
+                      clearWatchlistHandler(),
+                      Navigator.pop(context), // close alert window
+                    },
+                    child: Icon(
+                      Icons.check_circle_outline,
+                      size: 55,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.cancel_outlined,
+                      size: 55,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
       style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xFFFF0000),
           foregroundColor: const Color(0xFF800000)),
