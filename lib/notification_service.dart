50c08acb295b3fbb4bea0a7756a844babd9012bf
JoshMJohnson <joshjohns21@gmail.com>
2024-06-29 20:57:30 -0500

    code cleanup

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:awesome_notifications/android_foreground_service.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
@@ -149,6 +147,9 @@ class NotificationService {
   /* updates the current progress bar */
   static void updateProgressBar(
       int notificationID, int currentProgress, int totalTickersPulling) {
+    debugPrint(
+        'currentProgress: $currentProgress | totalTickersPulling: $totalTickersPulling'); // ! testing
+
     double progress = currentProgress / totalTickersPulling * 100;
 
     /* still pulling; reached api limit; delaying */
