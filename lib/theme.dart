d41fcdf9bce638762600614f32f1ff013a1421c3
JoshMJohnson <joshjohns21@gmail.com>
2024-07-07 17:30:36 -0500

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -89,61 +89,26 @@ ThemeData darkMode = ThemeData(
     primary:
         Color.fromARGB(255, 201, 201, 201), // bottom watchlist background color
     secondary: Color.fromARGB(255, 96, 7, 7), // top watchlist background color
-    tertiary: Color(0xFFE8EAF6),
+    tertiary: Color(0xFFE8EAF6), // todo discover where used
   ),
   dividerColor: const Color(0xFF90CAF9),
   textTheme: const TextTheme(
     headlineMedium: TextStyle(
-      color: Color.fromARGB(255, 154, 255, 154),
+      color: Color.fromARGB(255, 154, 255, 154), // header font
     ),
     bodyMedium: TextStyle(
-      color: Color.fromARGB(255, 154, 255, 154),
-      // normal font
+      color: Color.fromARGB(255, 154, 255, 154), // normal font
     ),
     bodySmall: TextStyle(
-      // bear red
-      color: Color(0xFFE57373),
+      color: Color(0xFFE57373), // bear red
     ),
     bodyLarge: TextStyle(
-      // bull green
-      color: Color(0xFF81C784),
+      color: Color(0xFF81C784), // bull green
     ),
   ),
-  hintColor: const Color.fromARGB(255, 154, 255, 154),
-  buttonTheme: const ButtonThemeData(
-    colorScheme: ColorScheme.light(
-      background: Colors.black,
-      primary: Color(0xFF1E88E5),
-      secondary: Colors.white,
-    ),
-  ),
-  sliderTheme: const SliderThemeData(
-    thumbColor: Color(0xFF42A5F5),
-    activeTrackColor: Color(0xFF9FA8DA),
-    inactiveTrackColor: Color(0xFF3F51B5),
-    disabledThumbColor: Color(0xFFE3F2FD),
-    disabledActiveTrackColor: Color(0xFF9FA8DA),
-    disabledInactiveTrackColor: Color(0xFF9FA8DA),
-  ),
-  dropdownMenuTheme: const DropdownMenuThemeData(
-    inputDecorationTheme: InputDecorationTheme(
-      iconColor: Color(0xFFBBDEFB),
-      fillColor: Colors.black,
-    ),
-  ),
-  radioTheme: RadioThemeData(fillColor:
-      MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-    return const Color(0xFFBBDEFB);
-  })),
-  iconTheme: const IconThemeData(
-    color: Color.fromARGB(255, 154, 255, 154),
-  ),
-  cardTheme: const CardTheme(
-    color: Color(0xFFE0E0E0),
-  ),
-  dialogTheme: const DialogTheme(
-    backgroundColor: Color(0xFF0B290A),
-  ),
+  hintColor: const Color.fromARGB(
+      255, 154, 255, 154), // text input field untouched - home page
+  // ticker symbol input field - home page
   inputDecorationTheme: const InputDecorationTheme(
     focusedBorder: UnderlineInputBorder(
       borderSide: BorderSide(
@@ -155,5 +120,42 @@ ThemeData darkMode = ThemeData(
         color: Color.fromARGB(255, 7, 88, 7),
       ),
     ),
+  ),
+  // buttons
+  buttonTheme: const ButtonThemeData(
+    colorScheme: ColorScheme.light(
+      background: Colors.black,
+      primary: Color(0xFF1E88E5),
+      secondary: Colors.white,
+    ),
+  ),
+  // slider - ticker page & settings
+  sliderTheme: const SliderThemeData(
+    thumbColor: Color(0xFF42A5F5),
+    activeTrackColor: Color(0xFF9FA8DA),
+    inactiveTrackColor: Color(0xFF3F51B5),
+    disabledThumbColor: Color(0xFFE3F2FD),
+    disabledActiveTrackColor: Color(0xFF9FA8DA),
+    disabledInactiveTrackColor: Color(0xFF9FA8DA),
+  ),
+  // dropdown - quantity reminders in settings
+  dropdownMenuTheme: const DropdownMenuThemeData(
+    inputDecorationTheme: InputDecorationTheme(
+      iconColor: Color(0xFFBBDEFB),
+      fillColor: Colors.black,
+    ),
+  ),
+  radioTheme: RadioThemeData(fillColor:
+      MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+    return const Color(0xFFBBDEFB);
+  })),
+  iconTheme: const IconThemeData(
+    color: Color.fromARGB(255, 154, 255, 154), // icons
+  ),
+  cardTheme: const CardTheme(
+    color: Color(0xFFE0E0E0),
+  ),
+  dialogTheme: const DialogTheme(
+    backgroundColor: Color(0xFF0B290A), // popup displays
   ),
 );
