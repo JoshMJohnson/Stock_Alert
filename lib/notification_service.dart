e2bf28479fcbe7b688f8234777c83ed8246c9d05
JoshMJohnson <joshjohns21@gmail.com>
2024-07-09 23:42:42 -0500

    potential foreground dismissing bug fix

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -131,6 +131,8 @@ class NotificationService {
         locked: true,
         autoDismissible: false,
         color: const Color.fromARGB(255, 70, 130, 180),
+        displayOnBackground: true,
+        displayOnForeground: true,
       ),
     );
   }
--- lib/pages/homePageWidgets/stock_watchlist.dart
+++ lib/pages/homePageWidgets/stock_watchlist.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:stock_alert/pages/homePageWidgets/stock_entity.dart';
 import '../ticker.dart';
