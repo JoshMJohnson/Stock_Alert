cd4e0577a2a07ced8afca195e43604f659bea81f
JoshMJohnson <joshjohns21@gmail.com>
2025-02-28 01:12:45 -0600

    Foreground service implemented

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart' as fgtask;
+import 'package:flutter_foreground_task/task_handler.dart';
 import 'package:stock_alert/database_repository.dart';
 import 'package:stock_alert/pages/homePageWidgets/stock_entity.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -31,6 +33,12 @@ Future<void> onDismissActionReceivedMethod(
 @pragma("vm:entry-point")
 Future<void> onActionReceivedMethod(ReceivedAction receivedAction) async {}
 
+// The callback function should always be a top-level or static function.
+@pragma('vm:entry-point')
+void startCallback() {
+  fgtask.FlutterForegroundTask.setTaskHandler(NotificationService());
+}
+
 @pragma(
     'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
 void callbackDispatcher() {
@@ -41,9 +49,9 @@ void callbackDispatcher() {
         "***************** Native called background task: $task"); //simpleTask will be emitted here.
     debugPrint(
         "***************** Native called background inputData: $inputData"); //simpleTask will be emitted here.
-    debugPrint('***** ${inputData!['counterID']}');
-    debugPrint('***** ${inputData['dayCounter']}');
-    debugPrint('***** ${inputData['notificationNum']}');
+    debugPrint('***** counterID: ${inputData!['counterID']}');
+    debugPrint('***** dayCounter: ${inputData['dayCounter']}');
+    debugPrint('***** notificationNum: ${inputData['notificationNum']}');
 
     String easternTimeZone = 'America/New_York';
     final int notificationNum = inputData['notificationNum'];
@@ -98,134 +106,23 @@ void callbackDispatcher() {
   });
 }
 
-class NotificationService {
-  /* initializes local notifications */
-  static Future init() async {
-    debugPrint('** init 1 **');
-
-    await channelCreation();
-    await createListeners();
-
-    debugPrint('** init 2 **');
-
-    Workmanager().initialize(
-      callbackDispatcher,
-    );
-
-    debugPrint('** init 3 **');
-  }
-
-  /* creates the event listeners for the notifications */
-  static Future createListeners() async {
-    AwesomeNotifications().setListeners(
-      onActionReceivedMethod: onActionReceivedMethod,
-      onDismissActionReceivedMethod: onDismissActionReceivedMethod,
-      onNotificationDisplayedMethod: onNotificationDisplayedMethod,
-      onNotificationCreatedMethod: onNotificationCreatedMethod,
-    );
-  }
-
-  /* creates the notification channels */
-  static Future channelCreation() async {
-    await AwesomeNotifications().initialize(
-      'resource://drawable/foreground_service_icon',
-      [
-        NotificationChannel(
-          groupKey: 'foreground_service',
-          channelKey: 'foreground_service',
-          channelName: 'Foreground Service',
-          channelDescription: 'Foreground service',
-          defaultPrivacy: NotificationPrivacy.Public,
-          playSound: false,
-          enableVibration: false,
-          locked: true,
-          importance: NotificationImportance.High,
-        ),
-        NotificationChannel(
-          groupKey: 'schedule_triggered',
-          channelKey: 'schedule_triggered',
-          channelName: 'Update Triggered (Required)',
-          channelDescription: 'Necessary for notification handling',
-          icon: 'resource://drawable/update_icon',
-          importance: NotificationImportance.Default,
-          playSound: false,
-          enableVibration: false,
-        ),
-        NotificationChannel(
-          groupKey: 'updating_stocks',
-          channelKey: 'update_progression',
-          channelName: 'Update Progression',
-          channelDescription: 'Progression on pulling updated ticker data',
-          icon: 'resource://drawable/update_icon',
-          importance: NotificationImportance.Low,
-          playSound: false,
-          enableVibration: false,
-        ),
-        NotificationChannel(
-          groupKey: 'bull_stocks',
-          channelKey: 'bull_channel',
-          channelName: 'Bull Stocks',
-          channelDescription: 'Provides alerts for stocks that are up.',
-          icon: 'resource://drawable/bull_icon',
-          defaultPrivacy: NotificationPrivacy.Public,
-          importance: NotificationImportance.Max,
-          playSound: true,
-          enableVibration: true,
-        ),
-        NotificationChannel(
-          groupKey: 'bear_stocks',
-          channelKey: 'bear_channel',
-          channelName: 'Bear Stocks',
-          channelDescription: 'Provides alerts for stocks that are down.',
-          icon: 'resource://drawable/bear_icon',
-          defaultPrivacy: NotificationPrivacy.Public,
-          importance: NotificationImportance.Max,
-          playSound: true,
-          enableVibration: true,
-        ),
-      ],
-    );
-  }
-
-  /* checks device settings if notifications are allowed */
-  static Future<bool> checkPermissions() async {
-    return await AwesomeNotifications().isNotificationAllowed();
-  }
-
-  /* promps user request for permissions */
-  static Future requestPermissions() async {
-    List<NotificationPermission> permissionList = [
-      NotificationPermission.Badge,
-      NotificationPermission.Alert,
-      NotificationPermission.Sound,
-      NotificationPermission.Vibration,
-      NotificationPermission.Light,
-      NotificationPermission.PreciseAlarms,
-    ];
-
-    // Check if the basic permission was granted by the user
-    await AwesomeNotifications().requestPermissionToSendNotifications(
-      permissions: permissionList,
-    );
-  }
-
-  /* starts the foreground service */
-  static startForegroundService() async {}
-
-  /* terminates the foreground service and terminates all previous scheduled notifications */
-  static terminateForegroundService() async {
+class NotificationService extends TaskHandler {
+  @override
+  Future<void> onDestroy(DateTime timestamp) async {
     AwesomeNotifications().cancelAll();
     Workmanager().cancelAll();
   }
 
-  /* 
-    creates a progression notification for pulling 
-    updated watchlist ticker data scheduled for a specific time 
-  */
-  static createScheduledProgression(
-    int quanitiyReminders,
-  ) async {
-    // String easternTimeZone = 'America/New_York';
+  @override
+  void onRepeatEvent(DateTime timestamp) {}
+
+  @override
+  Future<void> onStart(DateTime timestamp, fgtask.TaskStarter starter) async {
+    debugPrint('** onStart **');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final int quanitiyReminders = prefs.getInt('notificationQuantity')!;
 
     int counterID = 3;
     int dayCounter = 1;
@@ -469,6 +366,140 @@ class NotificationService {
         },
       );
     }
+  }
+
+  /* initializes local notifications */
+  static Future init() async {
+    await channelCreation();
+    await createListeners();
+    await initService();
+
+    Workmanager().initialize(
+      callbackDispatcher,
+    );
+  }
+
+  static Future initService() async {
+    fgtask.FlutterForegroundTask.initCommunicationPort();
+
+    fgtask.FlutterForegroundTask.init(
+      androidNotificationOptions: fgtask.AndroidNotificationOptions(
+        channelId: 'foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        onlyAlertOnce: true,
+      ),
+      iosNotificationOptions: const fgtask.IOSNotificationOptions(
+        showNotification: false,
+        playSound: false,
+      ),
+      foregroundTaskOptions: fgtask.ForegroundTaskOptions(
+        eventAction: fgtask.ForegroundTaskEventAction.nothing(),
+        autoRunOnBoot: true,
+        allowWakeLock: true,
+      ),
+    );
+  }
+
+  /* creates the event listeners for the notifications */
+  static Future createListeners() async {
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: onActionReceivedMethod,
+      onDismissActionReceivedMethod: onDismissActionReceivedMethod,
+      onNotificationDisplayedMethod: onNotificationDisplayedMethod,
+      onNotificationCreatedMethod: onNotificationCreatedMethod,
+    );
+  }
+
+  /* creates the notification channels */
+  static Future channelCreation() async {
+    await AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+          groupKey: 'schedule_triggered',
+          channelKey: 'schedule_triggered',
+          channelName: 'Update Triggered (Required)',
+          channelDescription: 'Necessary for notification handling',
+          icon: 'resource://drawable/update_icon',
+          importance: NotificationImportance.Default,
+          playSound: false,
+          enableVibration: false,
+        ),
+        NotificationChannel(
+          groupKey: 'updating_stocks',
+          channelKey: 'update_progression',
+          channelName: 'Update Progression',
+          channelDescription: 'Progression on pulling updated ticker data',
+          icon: 'resource://drawable/update_icon',
+          importance: NotificationImportance.Low,
+          playSound: false,
+          enableVibration: false,
+        ),
+        NotificationChannel(
+          groupKey: 'bull_stocks',
+          channelKey: 'bull_channel',
+          channelName: 'Bull Stocks',
+          channelDescription: 'Provides alerts for stocks that are up.',
+          icon: 'resource://drawable/bull_icon',
+          defaultPrivacy: NotificationPrivacy.Public,
+          importance: NotificationImportance.Max,
+          playSound: true,
+          enableVibration: true,
+        ),
+        NotificationChannel(
+          groupKey: 'bear_stocks',
+          channelKey: 'bear_channel',
+          channelName: 'Bear Stocks',
+          channelDescription: 'Provides alerts for stocks that are down.',
+          icon: 'resource://drawable/bear_icon',
+          defaultPrivacy: NotificationPrivacy.Public,
+          importance: NotificationImportance.Max,
+          playSound: true,
+          enableVibration: true,
+        ),
+      ],
+    );
+  }
+
+  /* checks device settings if notifications are allowed */
+  static Future<bool> checkPermissions() async {
+    return await AwesomeNotifications().isNotificationAllowed();
+  }
+
+  /* promps user request for permissions */
+  static Future requestPermissions() async {
+    List<NotificationPermission> permissionList = [
+      NotificationPermission.Badge,
+      NotificationPermission.Alert,
+      NotificationPermission.Sound,
+      NotificationPermission.Vibration,
+      NotificationPermission.Light,
+      NotificationPermission.PreciseAlarms,
+    ];
+
+    // Check if the basic permission was granted by the user
+    await AwesomeNotifications().requestPermissionToSendNotifications(
+      permissions: permissionList,
+    );
+  }
+
+  /* starts the foreground service */
+  static startForegroundService() async {
+    await fgtask.FlutterForegroundTask.startService(
+        serviceId: 1,
+        notificationTitle: 'Stock Alert is active...',
+        notificationText: 'Keeping you updated on the stock market',
+        notificationIcon: const fgtask.NotificationIcon(
+          metaDataName: 'com.stock_alert.service.ServiceIcon',
+        ),
+        callback: startCallback);
+  }
+
+  /* terminates the foreground service and terminates all previous scheduled notifications */
+  static terminateForegroundService() async {
+    await fgtask.FlutterForegroundTask.stopService();
   }
 
   /* updates the current progress bar */
